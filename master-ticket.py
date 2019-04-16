a3cc5a6f49bcbf08b58cb344f162e7ac938ec968
edward19 <edward19@abine.org>
2019-04-16 12:10:08 -0400

    updated

--- master-ticket.py
+++ master-ticket.py
@@ -13,8 +13,10 @@ number_of_tickets = input("Hi {}, how many tickets do you want?  "
 number_of_tickets = int(number_of_tickets)
 
 #Calculate the price and assign it to a variable
+
 total_price = TICKET_PRICE * number_of_tickets
 # total_price = TICKET_PRICE * int(number_of_tickets)
+
 print("Your total price is ${}".format(total_price))
 # print("Your total price is $" + str(total_price))
 
