538333e45ee93a09ebb483bca6c6b18cfa5ea6f1
edward19 <edward19@abine.org>
2019-04-16 13:36:37 -0400

    update

--- docs/use-cases.md
+++ docs/use-cases.md
@@ -1,7 +1,18 @@
 - Can I call ticketsAvailable()
+- Continuosly run as long as there are tickets available
+- Let the user know when tickets are sold out
 - Output number of tickets remaining
 - Get user's name and assign it to a variable
 - Prompt user by name and get number of tickets they want.
 - Calculate the price and assign it to a variable
    - Price is number of tickets multiplied by the price
 - Output the price to the screen
+
+- Prompt user to see if they want to proceed? Y/N
+- If they want to proceed...
+  - Print out to the screen "Sold!" to confirm purchase
+  - Get credit card info and process it
+  - Then reduce the number of tickets remaining by the number of tickets sold. 
+  - Otherwise...thank them by name
+  
+
--- master-ticket.py
+++ master-ticket.py
@@ -2,6 +2,7 @@ TICKET_PRICE = 10
 
 tickets_remaining = 100
 
+# Run code continuously until out of tickets_remaining
 print("There are {} tickets remaining.".format(tickets_remaining))
 
 #Get user's name and assign it to a variable
@@ -20,5 +21,13 @@ total_price = TICKET_PRICE * number_of_tickets
 print("Your total price is ${}".format(total_price))
 # print("Your total price is $" + str(total_price))
 
+confirm_purchase = input("Do you want to continue and purchase tickets? Type Y or N  ")
+# confirm_purchase = confirm_purchase.lower
 
-
+if confirm_purchase.lower == "y":
+    print("Sold! Thanks for shopping with us {}".format(name))
+    tickets_remaining -= number_of_tickets
+else:
+    print("Thanks for shopping with us and have a nice day {}".format(name))                      
+                         
+# Notifiy user tickets are sold out
